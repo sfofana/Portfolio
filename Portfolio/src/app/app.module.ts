b79e904a4ea432b5e16aa2e5a9ef1bf99a3770fe
sfofana <sufyanfofana@yahoo.com>
2020-02-14 19:52:21 -0600

    portfolio wire frame

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { NavbarModule, ChartsModule, WavesModule, ButtonsModule, CardsModule, ModalModule, TooltipModule, PopoverModule } from 'angular-bootstrap-md'
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home-page/home/home.component';
+import { WorkComponent } from './work-experience/work/work.component';
+import { SkillComponent } from './skill-matrix/skill/skill.component';
+import { ProjectsComponent } from './project/projects/projects.component';
+import { FreeComponent } from './freelance/free/free.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WorkComponent,
+    SkillComponent,
+    ProjectsComponent,
+    FreeComponent
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    NavbarModule,
+    WavesModule,
+    ButtonsModule,
+    ChartsModule,
+    CardsModule,
+    ModalModule,
+    TooltipModule,
+    PopoverModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Portfolio/src/app/skill-matrix/skill/skill.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skill',
+  templateUrl: './skill.component.html',
+  styleUrls: ['./skill.component.scss']
+})
+export class SkillComponent implements OnInit {
+
+  public chartType: string = 'doughnut';
+
+  //skillmatrix data sets
+  public chartDatasets1: Array<any> = [
+    { data: [120, 120, 120, 120, 120], label: 'Web Technologies' }
+  ];
+
+  public chartDatasets2: Array<any> = [
+    { data: [180, 180, 180, 120, 90], label: 'Core Framework' }
+  ];
+
+  public chartDatasets3: Array<any> = [
+    { data: [120, 120, 120, 120, 120], label: 'Server-Side' }
+  ];
+
+  public chartDatasets4: Array<any> = [
+    { data: [120, 120, 90, 90, 120], label: 'Testing Tools' }
+  ];
+
+  public chartDatasets5: Array<any> = [
+    { data: [540, 120, 120, 120, 120], label: 'Languages' }
+  ];
+
+  public chartDatasets6: Array<any> = [
+    { data: [90, 90, 90, 180, 90], label: 'DevOps' }
+  ];
+
+  public chartDatasets7: Array<any> = [
+    { data: [90, 90, 90, 90, 90], label: 'Web & Micro Services' }
+  ];
+
+  //labels
+  public chartLabels1: Array<any> = ['Angular', 'HTML', 'CSS', 'JavaScript', 'AJAX'];
+  public chartLabels2: Array<any> = ['Spring MVC', 'Spring ORM', 'Spring Cloud', 'Servlet', 'Hibernate'];
+  public chartLabels3: Array<any> = ['Apache Tomcat', 'NodeJS', 'JDBC', 'Oracle SQL', 'JSON'];
+  public chartLabels4: Array<any> = ['JUnit', 'Postman', 'Jasmine', 'Karma', 'Mockito'];
+  public chartLabels5: Array<any> = ['Java', 'HTML', 'JavaScript', 'TypeScript', 'XML'];
+  public chartLabels6: Array<any> = ['Jenkins', 'AWS EC2', 'AWS S3', 'Maven', 'Docker'];
+  public chartLabels7: Array<any> = ['Netflix Eureka', 'Netflix Hystrix', 'Netflix Zuul', 'REST', 'SOAP'];
+
+  public chartColors: Array<any> = [
+    {
+      backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'],
+      hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870', '#A8B3C5', '#616774'],
+      borderWidth: 2,
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public chartOptions: any = {
+    responsive: true
+  };
+  public chartClicked(e: any): void { }
+  public chartHovered(e: any): void { }
+}
